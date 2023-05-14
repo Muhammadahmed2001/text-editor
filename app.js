74f5b8bf55d679e4102cedb3b470f7d3d9e8eeb1
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-05-14 22:20:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var userInput = document.getElementById("user-input");
+function color() {
+    var color = document.getElementById("color").value;
+    userInput.style.color = color;
+}
+function textSize (){
+    var size = document.getElementById("text-size").value;
+    userInput.style.fontSize = size+"px";
+
+    // console.log(size);
+    
+}
