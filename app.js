969e8b90e42e7916f2ccec3d2f0657bcd518dd96
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-05-19 01:14:40 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ function textSize (){
     var size = document.getElementById("text-size").value;
     userInput.style.fontSize = size+"px";
 
-    // console.log(size);
+}
+function fontFamily(){
+    var fontFamily = document.getElementById("font-family").value
+    if(fontFamily === "GreatVibes-Regular"){
+        userInput.style.fontFamily = "firstFont"
+    }
+    else if(fontFamily === "IndieFlower-Regular"){
+        userInput.style.fontFamily = "secoundFont"
+    }
+    else if(fontFamily === "Italianno-Regular"){
+        userInput.style.fontFamily = "thirdFont"
+    }
+    else if(fontFamily === "Pacifico-Regular"){
+        userInput.style.fontFamily = "fourthFont"
+    }
+    else if(fontFamily === "PermanentMarker-Regular"){
+        userInput.style.fontFamily = "fifthFont"
+    }
+    else{
+        userInput.style.fontFamily = "none"
+    }
     
 }
